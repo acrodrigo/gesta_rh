297b079457f8c0567476dfa6ab889ad41a9d7ca1
acrodrigo <alencarcosta3@gmail.com>
2019-12-28 12:37:21 -0300

    relacionamentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
-    descricao = models.CharField(max_length=100,)
+    descricao = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.descricao

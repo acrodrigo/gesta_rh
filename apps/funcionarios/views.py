a550b03638296e8a85ff038f8dd9b6c5ace560bf
acrodrigo <alencarcosta3@gmail.com>
2020-07-21 21:51:31 -0300

    Aula 23 - Crud Funcionario, Create

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 
 from .models import Funcionario
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 
 class FuncionarioList(ListView):
     model = Funcionario
@@ -10,6 +11,19 @@ class FuncionarioList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioNovo(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioNovo, self).form_valid(form)
 
 
 class FuncionarioEdit(UpdateView):

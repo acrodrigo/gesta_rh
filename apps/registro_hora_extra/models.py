c5ad7942827fc95c57f55cd6e6ef1342d700ac8b
acrodrigo <alencarcosta3@gmail.com>
2020-07-25 15:31:37 -0300

    Aula 33 - Update Registro Hora Extra

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.funcionarios.models import Funcionario
+from django.urls import reverse
 
 
 class RegistroHoraExtra(models.Model):
@@ -7,6 +8,8 @@ class RegistroHoraExtra(models.Model):
     funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)    
     horas = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def get_absolute_url(self):
+        return reverse('list_hora_extra')
 
     def __str__(self):
         return self.motivo
--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('list', views.HoraExtraList.as_view(), name="list_hora_extra"),
+    path('editar/<int:pk>', views.HoraExtraEdit.as_view() ,name="update_hora_extra")
 ]

03eba03d8c6ecb96d5e3a2ebf5d16966ea4329f3
acrodrigo <alencarcosta3@gmail.com>
2020-07-28 20:44:22 -0300

    Aula 49 - Criando o calculo de banco de horas

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from django.db.models import Sum
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
 
@@ -11,6 +11,10 @@ class Funcionario(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    @property
+    def total_horas_extras(self):
+        return self.registrohoraextra_set.all().aggregate(Sum('horas'))['horas__sum']
 
     def get_absolute_url(self):
         return reverse('list_funcionarios')
